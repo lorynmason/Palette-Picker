baa7e9290079d9c9f3a3d75b63403a815b225f0d
Loryn Mason <masonloryn@gmail.com>
2019-01-23 18:04:21 -0700

    Fix margins, and alignment

--- server.js
+++ server.js
@@ -3,6 +3,42 @@ const app = express();
 
 app.use(express.static('public'));
 
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Palette Picker';
+
+app.locals.projects = [
+  { id: '1', name: 'Project 1'},
+  { id: '2', name: 'Project 2'},
+  { id: '3', name: 'Project 3' }
+];
+
+app.locals.palettes = [
+  { id: '1', name: 'Palette 1', project_id: 1},
+  { id: '2', name: 'Palette 2', project_id: 1},
+  { id: '3', name: 'Palette 3', project_id: 2 },
+  { id: '4', name: 'Palette 4', project_id: 2 },
+  { id: '5', name: 'Palette 5', project_id: 3 },
+];
+
+app.get('/api/projects', (request, response) => {
+  const { projects } = app.locals
+  return response.json({ projects });
+});
+
+app.post('/api/projects', (request, response) => {
+  const id = Date.now();
+  const project = request.body.project;
+
+  if (!project) {
+    return response.status(422).send({
+      error: 'No project property provided'
+    });
+  } else {
+    app.locals.projects.push({ project });
+    return response.status(201).json({ id });
+  }
+});
+
 app.listen(3000, () => {
   console.log('Express Intro running on localhost:3000');
 });
